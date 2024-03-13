61d1bdcb89470d2dadf0b0ab028e6fe76f2ac079
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-03-13 23:10:20 +0100

    wip

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+
+
+import { getSession } from 'auth-astro/server';
+
+
+const protectedRoutes: Set<string> = new Set([
+    "/dashboard"
+]);
+
+export async function onRequest ({ locals, request }:any, next:any) {
+    // intercept data from a request
+    // optionally, modify the properties in `locals`
+    locals.title = "New title";
+
+    const session = await getSession(request);
+
+    if ( !session && protectedRoutes.has(new URL(request.url).pathname) ) {
+        console.log("no logged for ", new URL(request.url).pathname)
+
+        return Response.redirect(new URL("/login", request.url), 302);
+    }
+
+
+
+
+
+
+    return next();
+
+    // if (!session) {
+    //     console.log(request.url);
+    //     return Response.redirect(new URL("/", request.url), 302);
+    // } else {
+    //     return next();
+    // }
+};
+
