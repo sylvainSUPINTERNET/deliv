7345d90add92aae2e1845d86f27f07ae2b5d6bbe
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-03-24 23:14:17 +0100

    [WIP]

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 import cloudflare from "@astrojs/cloudflare";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(), auth(), react()],
   output: "server",
   adapter: cloudflare()
 });
---
+++ src/components/dashboard/Button.tsx
@@ -0,0 +1,6 @@
+
+export function Button () {
+    return (
+        <button>Click me!</button>
+    )
+}
